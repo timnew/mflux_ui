f70a2b95df3e1481c3ef6f3743daa22c35fefaba
timnew <1287238+timnew@users.noreply.github.com>
2024-09-09 00:27:33 +1000

    Fix app name

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'MFlux UI',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
