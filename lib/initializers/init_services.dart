54a040b69849eabdeaef594c4ceac253dd63184f
timnew <1287238+timnew@users.noreply.github.com>
2024-09-11 00:41:50 +1000

    Fix init failed

--- lib/initializers/init_services.dart
+++ lib/initializers/init_services.dart
@@ -2,6 +2,7 @@ import 'package:app_preference/app_preference_plugin_interface.dart';
 import 'package:app_preference_shared_preferences/app_preference_shared_preferences.dart';
 import 'package:mfluxui/features/find_service.dart';
 import 'package:mfluxui/features/open_ai.dart';
+import 'package:mfluxui/features/output.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'create_app_preferences.dart';
@@ -15,20 +16,24 @@ Future<void> initServices() async {
 
   findService.registerSingletonWithDependencies(
     () => createOpenAiSettingsPreference(findService()),
-    dependsOn: [SharedPreferencesAdapter],
+    dependsOn: [AppPreferenceAdapter],
   );
   findService.registerSingletonWithDependencies(
     () => createGenerationOptionsPreference(findService()),
-    dependsOn: [SharedPreferencesAdapter],
+    dependsOn: [AppPreferenceAdapter],
   );
   findService.registerSingletonWithDependencies(
     () => createOutputSettingsPreference(findService()),
-    dependsOn: [SharedPreferencesAdapter],
+    dependsOn: [AppPreferenceAdapter],
   );
 
   findService.registerSingletonWithDependencies(
     () => OpenAiRepository(findService()),
     dependsOn: [OpenAiSettingsPreference],
+  );
+  findService.registerSingletonWithDependencies(
+    () => OutputRepository(findService()),
+    dependsOn: [OutputSettingsPreference],
   );
 
   await findService.allReady();
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:mfluxui/app/app.dart';
 import 'initializers/init_services.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await initServices();
 
   runApp(const App());
